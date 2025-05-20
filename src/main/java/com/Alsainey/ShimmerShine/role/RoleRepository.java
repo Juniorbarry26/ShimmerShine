b03a133531eca692a008c88d78a6cd49b7387b4b
Alsainey Barry <alsainey7@gmail.com>
2025-05-20 16:36:46 +0000

    Create the role repository class

--- src/main/java/com/Alsainey/ShimmerShine/role/RoleRepository.java
+++ src/main/java/com/Alsainey/ShimmerShine/role/RoleRepository.java
@@ -1,2 +1,9 @@
-package com.Alsainey.ShimmerShine.role;public interface RoleRepository {
+package com.Alsainey.ShimmerShine.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
 }
