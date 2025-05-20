51f8b93ff742ddaa4f746e39c1ba541ec2a55659
Alsainey Barry <alsainey7@gmail.com>
2025-05-20 16:39:30 +0000

    Create the user repository class

--- src/main/java/com/Alsainey/ShimmerShine/user/UserRepository.java
+++ src/main/java/com/Alsainey/ShimmerShine/user/UserRepository.java
@@ -1,2 +1,10 @@
-package com.Alsainey.ShimmerShine.user;public interface UserRepository {
+package com.Alsainey.ShimmerShine.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
 }
