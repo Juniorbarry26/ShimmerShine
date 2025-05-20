f4c01e8c7613b131034fa102341f74487bde460b
Alsainey Barry <alsainey7@gmail.com>
2025-05-20 16:06:29 +0000

    Initial commit for the backend structure

---
+++ src/main/java/com/Alsainey/ShimmerShine/ShimmerShineApplication.java
@@ -0,0 +1,13 @@
+package com.Alsainey.ShimmerShine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShimmerShineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShimmerShineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Alsainey/ShimmerShine/ShimmerShineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alsainey.ShimmerShine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShimmerShineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
