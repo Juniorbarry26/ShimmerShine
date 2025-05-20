49242eb80f0e485806a4f7b7de8945ee3cf78a48
Alsainey Barry <alsainey7@gmail.com>
2025-05-20 16:37:44 +0000

    Create the token class with token details

--- src/main/java/com/Alsainey/ShimmerShine/user/Token.java
+++ src/main/java/com/Alsainey/ShimmerShine/user/Token.java
@@ -1,2 +1,27 @@
-package com.Alsainey.ShimmerShine.user;public class Token {
+package com.Alsainey.ShimmerShine.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String token;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private LocalDateTime validatedAt;
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
 }
