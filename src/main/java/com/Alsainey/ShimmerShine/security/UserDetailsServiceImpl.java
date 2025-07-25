fe7ae25f1dc2682639c5e96a955f056f7aee43a8
Alsainey Barry <alsainey7@gmail.com>
2025-07-25 03:17:42 +0000

    feat(security): add security implementation classes and filters

---
+++ src/main/java/com/Alsainey/ShimmerShine/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.Alsainey.ShimmerShine.security;
+
+import com.Alsainey.ShimmerShine.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow();
+    }
+}
