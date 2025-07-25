843dc127598b8507b8731837292101fa40bc6d77
Alsainey Barry <alsainey7@gmail.com>
2025-07-25 03:15:38 +0000

    chore: update main application class

--- src/main/java/com/Alsainey/ShimmerShine/ShimmerShineApplication.java
+++ src/main/java/com/Alsainey/ShimmerShine/ShimmerShineApplication.java
@@ -1,13 +1,30 @@
 package com.Alsainey.ShimmerShine;
 
+import com.Alsainey.ShimmerShine.role.Role;
+import com.Alsainey.ShimmerShine.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-public class ShimmerShineApplication {
+@EnableJpaAuditing
+@EnableAsync
+ public class ShimmerShineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShimmerShineApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+
+	}
 }
