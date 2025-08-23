808064d3dd66574e7fbbd5e2aa48e3d996d97e93
Alsainey Barry <ab22316053@utg.edu.gm>
2025-08-23 00:47:31 +0000

    feat: add security context to the swagger docs

--- src/main/java/com/Alsainey/ShimmerShine/config/OpenApiConfig.java
+++ src/main/java/com/Alsainey/ShimmerShine/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.Alsainey.ShimmerShine.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,10 +18,20 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("ShimmerShine Backend API")
                         .version("1.0")
-                        .description("Backend structure for ShimmerShine")
+                        .description("Backend Structure for ShimmerShine")
                         .contact(new Contact()
                                 .name("Alsainey Barry")
                                 .email("alsainey7@gmail.com")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
                         )
                 );
     }
