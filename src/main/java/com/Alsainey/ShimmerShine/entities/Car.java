42aa5b35341df1c2539100fdeb03222db8b31b5a
Alsainey Barry <ab22316053@utg.edu.gm>
2025-08-31 04:23:18 +0000

    refactor: replace the car entity for user mapping to eager instead of Lazy loading to get all the user data

--- src/main/java/com/Alsainey/ShimmerShine/entities/Car.java
+++ src/main/java/com/Alsainey/ShimmerShine/entities/Car.java
@@ -18,7 +18,7 @@ public class Car {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY) // Many cars can belong to one user
+    @ManyToOne(fetch = FetchType.EAGER) // Many cars can belong to one user
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
