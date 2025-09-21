b44bb990eee5a158a11479db5d15614f8325631b
Alsainey Barry <ab22316053@utg.edu.gm>
2025-09-21 15:20:08 +0000

    feat: implement the attributes for the payment module

---
+++ src/main/java/com/Alsainey/ShimmerShine/entities/payment/enums/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.Alsainey.ShimmerShine.entities.payment.enums;
+
+public enum PaymentMethod {
+    CARD,
+    CASH,
+    MOBILE_MONEY,
+    PAYPAL, //optional for future implementations
+    STRIPE //optional for future implementations
+}
